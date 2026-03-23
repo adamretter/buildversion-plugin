7d63977f80142abeef4dd74f2bec6b66f43bf7c9
Adam Retter <adam.retter@googlemail.com>
2026-03-23 23:13:05 +0000

    Add license header to source files

--- src/it/simple-project/setup.groovy
+++ src/it/simple-project/setup.groovy
@@ -1,3 +1,15 @@
+/*
+ * Build Version Maven Plugin - This is a maven plugin that extracts current build information from git
+    projects, including: the latest commit hash, timestamp, most recent tag,
+    number of commits since most recent tag. It also implements a "follow first
+    parent" flavor of git describe.
+ * Copyright © 2012 Fernando Dobladez
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 // make this test project a "git" repository by copying the .git directory
 // from the sample project used on unit tests
 
